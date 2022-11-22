5a3265d918d157081a4f9feb4cb66002be5c067b
Jeanneret Aurélie <jeanaur@divtec.ch>
2022-11-22 12:53:13 +0100

    Composant : Client.vue = OK

--- exercices-quasar/src/components/Client.vue
+++ exercices-quasar/src/components/Client.vue
@@ -1,124 +1,52 @@
 <template>
-  <q-card
-    class="card">
-    <q-img
-      :src="clients.image ? clients.image: 'statics/image-placeholder.png'"
-      basic
-      contain
+  <q-page padding>
+    <h3>Liste des clients</h3>
+    <q-list
+      class="rounded-borders"
+      bordered
+      separator
     >
-      <div class="absolute-bottom text-h6">
-        {{ clients.nom }}
-      </div>
-    </q-img>
+      <!-- Un élément de liste, un client -->
+      <q-item clickable v-ripple>
+        <!-- Photo du client -->
+        <q-item-section avatar>
+          <q-avatar>
+            <img :src="clients.picture.thumbnail">
+          </q-avatar>
+        </q-item-section>
 
-    <q-card-section>
-      <q-rating
-        :model-value="clients.note"
-        size="2em"
-        color="orange"
-        readonly
-        class="q-mt-sm"
-      />
-    </q-card-section>
+        <!-- Nom, prénom et email du client -->
+        <q-item-section>
+          <q-item-label lines="1">
+            {{ clients.name.last }} {{ clients.name.first }}
+          </q-item-label>
+          <q-item-label caption lines="2">
+            {{ clients.email }}
+          </q-item-label>
+        </q-item-section>
 
-    <q-card-section>
-      <span v-if="clients.description">{{ plat.description }}</span>
-      <i v-else>Aucune description fournie.</i>
-    </q-card-section>
-
-    <q-card-actions
-      class="absolute-bottom"
-      align="right">
-      <q-btn
-        @click.stop="afficherFormPlat = true"
-        dense
-        round
-        icon="edit"
-        color="blue"
-        flat>Modifier</q-btn>
-      <q-btn
-        @click.stop="dialogSupprimer(plat.id)"
-        icon="delete"
-        color="red"
-        flat>Supprimer</q-btn>
-    </q-card-actions>
-
-    <q-dialog
-      v-model="afficherFormPlat">
-      <form-plat
-        action="modifier"
-        :platAModifier="plat"
-        @close="afficherFormPlat = false" />
-    </q-dialog>
-  </q-card>
+        <!-- Icône email -->
+        <q-item-section side>
+          <q-icon name="email" color="primary"/>
+        </q-item-section>
+      </q-item>
+    </q-list>
+  </q-page>
 </template>
 
 <script>
-// importation des fonctions utilitaires
-import { mapActions } from 'vuex'
 
 export default {
   // eslint-disable-next-line vue/multi-word-component-names
-  name: 'Plat',
-  props: ['plat'],
-  data () {
-    return {
-      afficherFormPlat: false
+  name: 'Client',
+  props: {
+    clients: {
+      type: Object,
+      required: true
     }
-  },
-  methods: {
-    // Mappage des actions
-    ...mapActions('plats', ['supprimerPlat']),
-    // Ouvre une boite de dialog pour confirmer la suppression
-    dialogSupprimer (id) {
-      this.$q.dialog({
-        title: 'Supprimer plat',
-        message: 'Voulez-vous vraiment supprimer ce plat ?',
-        persistent: true,
-        ok: {
-          label: 'Supprimer',
-          push: true
-        },
-        cancel: {
-          label: 'Annuler',
-          push: true,
-          color: 'grey'
-        }
-      }).onOk(() => {
-        this.supprimerPlat(this.plat.id)
-      })
-    }
-  },
-
-  components: {
-    'form-plat': require('components/FormPlat.vue').default
   }
 }
 </script>
 
 <style>
-.card {
-  min-height: 400px;
-  max-width: 250px;
-  width: 250px;
-  transition: background 0.3s;
-}
-.card-clickable {
-  cursor: pointer;
-}
-.card-clickable:hover {
-  background: #bdbdbd!important;
-}
-.card .q-img {
-  max-height: 180px;
-}
-.card .q-img__image {
-  background-size: cover !important;
-}
-.card .q-rating__icon {
-  opacity: 0.2;
-}
-.card .q-rating__icon--active {
-  opacity: 1;
-}
 </style>
